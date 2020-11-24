87f025c1068f09a6198dbb1bef6da929f22cb37f
Lisanna Dettwyler <lisanna@galois.com>
2020-11-24 14:12:49 -0800

    test

--- helpers.sh
+++ helpers.sh
@@ -81,68 +81,57 @@ setout() {
     fi
 }
 
-fetch-url() { (
-    set -Eeuo pipefail
-    name=$1
-    url=$2
-    setout $name "${FUNCNAME[0]} $url" && echo $out && return 0 || :
-    curl -o $out -L $url >&2
-    echo $out
-) }
-
 fetch-zip() { (
     set -Eeuo pipefail
-    name=$1
+    out=$1
     url=$2
     setout $name "${FUNCNAME[0]} $url" && echo $out && return 0 || :
     tmp=$(mktemp -d)
-    curl -o $tmp/file.zip -L $url >&2
-    unzip $tmp/file.zip -d $tmp/unpacked >&2
+    curl -o $tmp/file.zip -L $url
+    unzip $tmp/file.zip -d $tmp/unpacked
     if [[ $(ls -A1 $tmp/unpacked | wc -l) == 1 ]]; then
         mv $tmp/unpacked/$(ls -A1 $tmp/unpacked) $out
     else
         mv $tmp/unpacked $out
     fi
-    rm -rf $tmp
-    echo $out
-) }
+    rm -rf "$tmp"
+) >&2 ; }
 
 fetch-tarball() { (
     set -Eeuo pipefail
-    name=$1
+    out=$1
     url=$2
-    setout $name "${FUNCNAME[0]} $url" && echo $out && return 0 || :
     tmp=$(mktemp -d)
-    curl -o $tmp/file -L $url >&2
-    mkdir $tmp/unpacked
-    tar xfz $tmp/file -C $tmp/unpacked
-    if [[ $(ls -A1 $tmp/unpacked | wc -l) == 1 ]]; then
-        mv $tmp/unpacked/$(ls -A1 $tmp/unpacked) $out
+    curl -o "$tmp/file" -L "$url" >&2
+    mkdir "$tmp/unpacked"
+    tar xfz "$tmp/file" -C "$tmp/unpacked"
+    if [[ "$(ls -A1 $tmp/unpacked | wc -l)" == 1 ]]; then
+        mv "$tmp/unpacked/$(ls -A1 "$tmp/unpacked")" "$out"
     else
-        mv $tmp/unpacked $out
+        mv "$tmp/unpacked" "$out"
     fi
-    rm -rf $tmp
-    echo $out
-) }
+    rm -rf "$tmp"
+) >&2 ; }
 
 get-z3() { (
     set -Eeuo pipefail
-    platform=$1
-    version=$2
+    out=$1
+    platform=$2
+    version=$3
     case $platform in
       Linux | ubuntu | ubuntu-xenial) file="ubuntu-16.04.zip" ;;
       macOS | macos-10.14 | macos-mojave) file="osx-10.14.6.zip" ;;
       Windows) file="win.zip" ;;
       *) echo "unrecognized platform $platform" >&2 && return 1 ;;
     esac
-    fetch-zip z3 https://github.com/Z3Prover/z3/releases/download/z3-$version/z3-$version-x64-$file
+    fetch-zip  https://github.com/Z3Prover/z3/releases/download/z3-$version/z3-$version-x64-$file
 ) }
 
 get-cvc4() { (
     set -Eeuo pipefail
-    platform=$1
-    version=$2
-    setout cvc4 "${FUNCNAME[0]} $platform $version" && echo $out && return 0 || :
+    out=$1
+    platform=$2
+    version=$3
     EXT=""
     case $platform in
       Linux) file="x86_64-linux-opt" ;;
@@ -151,26 +140,26 @@ get-cvc4() { (
       *) echo "unrecognized platform $platform" >&2 && return 1 ;;
     esac
     mkdir -p $out/bin
-    mv $(fetch-url $file "https://github.com/CVC4/CVC4/releases/download/$version/cvc4-$version-$file") $out/bin/cvc4$EXT
+    curl -o "$out/bin/cvc4$EXT" -L "https://github.com/CVC4/CVC4/releases/download/$version/cvc4-$version-$file" >&2
     [ -z "$EXT" ] && chmod +x $out/bin/cvc4
     echo $out
 ) }
 
 get-yices() { (
     set -Eeuo pipefail
-    platform=$1
-    version=$2
-    ext=".tar.gz"
+    out=$1
+    platform=$2
+    version=$3
     case $platform in
       Linux) file="pc-linux-gnu-static-gmp.tar.gz" ;;
       macOS) file="apple-darwin18.7.0-static-gmp.tar.gz" ;;
-      Windows) file="pc-mingw32-static-gmp.zip" && ext=".zip" ;;
+      Windows) file="pc-mingw32-static-gmp.zip" ;;
       *) echo "unrecognized platform $platform" >&2 && return 1 ;;
     esac
-    if [[ $ext == ".tar.gz" ]]; then
-        fetch-tarball yices "https://yices.csl.sri.com/releases/$version/yices-$version-x86_64-$file"
+    if [[ "$platform" == Windows ]]; then
+        fetch-zip $out "https://yices.csl.sri.com/releases/$version/yices-$version-x86_64-$file"
     else
-        fetch-zip yices "https://yices.csl.sri.com/releases/$version/yices-$version-x86_64-$file"
+        fetch-tarball $out "https://yices.csl.sri.com/releases/$version/yices-$version-x86_64-$file"
     fi
 )}
 
@@ -183,13 +172,22 @@ setup-solvers-github() {
     Z3_VERSION=${Z3_VERSION:-""}
     CVC4_VERSION=${CVC4_VERSION:-""}
     YICES_VERSION=${YICES_VERSION:-""}
-    [ -z "$Z3_VERSION" ] || get-z3 $RUNNER_OS $Z3_VERSION >/dev/null &
-    [ -z "$CVC4_VERSION" ] || get-cvc4 $RUNNER_OS $CVC4_VERSION >/dev/null &
-    [ -z "$YICES_VERSION" ] || get-yices $RUNNER_OS $YICES_VERSION >/dev/null &
+    if [ -z "$Z3_VERSION" ]; then
+        export Z3_ROOT=$(mktemp -d)
+        get-z3 $Z3_ROOT $RUNNER_OS $Z3_VERSION &
+    fi
+    if [ -z "$CVC4_VERSION" ]; then
+        export CVC4_ROOT=$(mktemp -d)
+        get-cvc4 $CVC4_ROOT $RUNNER_OS $CVC4_VERSION &
+    fi
+    if [ -z "$YICES_VERSION" ]; then
+        export YICES_ROOT=$(mktemp -d)
+        get-yices $YICES_ROOT $RUNNER_OS $YICES_VERSION &
+    fi
     wait
-    [ -z "$Z3_VERSION" ] || add-path-github $(get-z3 $RUNNER_OS $Z3_VERSION)/bin
-    [ -z "$CVC4_VERSION" ] || add-path-github $(get-cvc4 $RUNNER_OS $CVC4_VERSION)/bin
-    [ -z "$YICES_VERSION" ] || add-path-github $(get-yices $RUNNER_OS $YICES_VERSION)/bin
+    [ -z "$Z3_VERSION" ] || add-path-github $Z3_ROOT/bin
+    [ -z "$CVC4_VERSION" ] || add-path-github $CVC4_ROOT/bin
+    [ -z "$YICES_VERSION" ] || add-path-github $YICES_ROOT/bin
 }
 
 if test "$#" -gt 0; then
