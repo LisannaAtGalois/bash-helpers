31e77f1c78a9d8fde13fea47a3bdd10539b006e1
Lisanna Dettwyler <lisanna@galois.com>
2020-11-30 15:22:12 -0800

    update

--- helpers.sh
+++ helpers.sh
@@ -1,30 +1,31 @@
 #! /usr/bin/env bash
 
-gha-add-path() {
+github-add-path() {
     echo "$1" >> "$GITHUB_PATH"
+    export PATH="$PATH:$1"
 }
 
-gha-set-env() {
+github-set-env() {
     echo "$1=$2" >> "$GITHUB_ENV"
+    eval "$1"="$2"
+    export "${1?}"
 }
 
-fetch-zip() { (
-    set -Eeuo pipefail
+fetch-zip() {
     out=$1
     url=$2
     tmp=$(mktemp -d)
-    curl -o "$tmp/file.zip" -L "$url"
-    unzip "$tmp/file.zip" -d "$tmp/unpacked"
+    curl -o "$tmp/file.zip" -L "$url" >&2
+    unzip "$tmp/file.zip" -d "$tmp/unpacked" >&2
     if [[ $(ls -A1 "$tmp/unpacked" | wc -l) == 1 ]]; then
-        mv "$tmp/unpacked/$(ls -A1 $tmp/unpacked)" $out
+        mv "$tmp/unpacked/$(ls -A1 $tmp/unpacked)" "$out"
     else
         mv "$tmp/unpacked" "$out"
     fi
     rm -rf "$tmp"
-) >&2 ; }
+}
 
-fetch-tarball() { (
-    set -Eeuo pipefail
+fetch-tarball() {
     out=$1
     url=$2
     tmp=$(mktemp -d)
@@ -37,10 +38,9 @@ fetch-tarball() { (
         mv "$tmp/unpacked" "$out"
     fi
     rm -rf "$tmp"
-) >&2 ; }
+}
 
-get-z3() { (
-    set -Eeuo pipefail
+get-z3() {
     out=$1
     platform=$2
     version=$3
@@ -51,10 +51,9 @@ get-z3() { (
       *) echo "unrecognized platform $platform" >&2 && return 1 ;;
     esac
     fetch-zip "$out" "https://github.com/Z3Prover/z3/releases/download/z3-$version/z3-$version-x64-$file"
-) }
+}
 
-get-cvc4() { (
-    set -Eeuo pipefail
+get-cvc4() {
     out=$1
     platform=$2
     version=$3
@@ -68,10 +67,9 @@ get-cvc4() { (
     mkdir -p "$out/bin"
     curl -o "$out/bin/cvc4$EXT" -L "https://github.com/CVC4/CVC4/releases/download/$version/cvc4-$version-$file" >&2
     [ -n "$EXT" ] || chmod +x "$out/bin/cvc4"
-) }
+}
 
-get-yices() { (
-    set -Eeuo pipefail
+get-yices() {
     out=$1
     platform=$2
     version=$3
@@ -86,37 +84,54 @@ get-yices() { (
     else
         fetch-tarball "$out" "https://yices.csl.sri.com/releases/$version/yices-$version-x86_64-$file"
     fi
-)}
-
-add-path-github() {
-    echo "$1" >> "$GITHUB_PATH"
-    export PATH="$PATH:$1"
 }
 
-setup-solvers-github() {
+setup-solvers() {
+    # Input
     Z3_VERSION=${Z3_VERSION:-""}
     CVC4_VERSION=${CVC4_VERSION:-""}
     YICES_VERSION=${YICES_VERSION:-""}
-    solvers=$(mktemp -d)
+    SOLVERS_DIR=${SOLVERS_DIR:-$(mktemp -d)}
+
     if [ -n "$Z3_VERSION" ]; then
-        Z3_ROOT="$solvers/z3"
-        export Z3_ROOT
-        add-path-github "$Z3_ROOT/bin"
-        get-z3 "$Z3_ROOT" "$RUNNER_OS" "$Z3_VERSION" &
+        Z3_ROOT="$SOLVERS_DIR/z3"
+        [ ! -d "$Z3_ROOT" ] || get-z3 "$Z3_ROOT" "$RUNNER_OS" "$Z3_VERSION" &
     fi
     if [ -n "$CVC4_VERSION" ]; then
-        CVC4_ROOT="$solvers/cvc4"
-        export CVC4_ROOT
-        add-path-github "$CVC4_ROOT/bin"
-        get-cvc4 "$CVC4_ROOT" "$RUNNER_OS" "$CVC4_VERSION" &
+        CVC4_ROOT="$SOLVERS_DIR/cvc4"
+        [ ! -d "$CVC4_ROOT" ] || get-cvc4 "$CVC4_ROOT" "$RUNNER_OS" "$CVC4_VERSION" &
     fi
     if [ -n "$YICES_VERSION" ]; then
-        YICES_ROOT="$solvers/yices"
-        export YICES_ROOT
-        add-path-github "$YICES_ROOT/bin"
-        get-yices "$YICES_ROOT" "$RUNNER_OS" "$YICES_VERSION" &
+        YICES_ROOT="$SOLVERS_DIR/yices"
+        [ ! -d "$YICES_ROOT" ] || get-yices "$YICES_ROOT" "$RUNNER_OS" "$YICES_VERSION" &
     fi
     wait
+
+    # Check that all requested solvers are usable or fail
+    [ -z "$Z3_VERSION" ] || "$Z3_ROOT/bin/z3" --version >/dev/null 2>&1 || return 1
+    [ -z "$CVC4_VERSION" ] || "$CVC4_ROOT/bin/cvc4" --version >/dev/null 2>&1 || return 1
+    [ -z "$YICES_VERSION" ] || "$YICES_ROOT/bin/yices" --version >/dev/null 2>&1 || return 1
+
+    # Output
+    [ -n "$Z3_VERSION" ] && export Z3_ROOT
+    [ -n "$CVC4_VERSION" ] && export CVC4_ROOT
+    [ -n "$YICES_VERSION" ] && export YICES_ROOT
+}
+
+github-setup-solvers() {
+    setup-solvers || return 1
+    if [ -n "$Z3_ROOT" ]; then
+        github-add-path "$Z3_ROOT/bin"
+        github-set-env Z3_ROOT "$Z3_ROOT"
+    fi
+    if [ -n "$CVC4_ROOT" ]; then
+        github-add-path "$CVC4_ROOT/bin"
+        github-set-env CVC4_ROOT "$CVC4_ROOT"
+    fi
+    if [ -n "$YICES_ROOT" ]; then
+        github-add-path "$YICES_ROOT/bin"
+        github-set-env YICES_ROOT "$YICES_ROOT"
+    fi
 }
 
 if [ "$#" -gt 0 ]; then
